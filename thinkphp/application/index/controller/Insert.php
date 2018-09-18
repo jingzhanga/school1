1780c1cabbe50999584febdd9159b7c50a363b0b
学校 <37288505+jingzhanga@users.noreply.github.com>
2018-09-19 00:24:22 +0800

    updata

--- thinkphp/application/index/controller/Insert.php
+++ thinkphp/application/index/controller/Insert.php
@@ -8,7 +8,22 @@ header("content-type:text/html;charset=utf-8");
 class Insert extends Controller
 {
     public function index(){
+    	 $t=Session::get('name');
+    	 	if(empty($t)){
+    	 		 $this->redirect(url('Login/index'));
+    	 	}
+    	 $dafen	=$t[0]['Use_ID'];
     	 $param =input('post.');
-    	 var_dump($param);
+    	 foreach ($param as $key => $value) {
+    	 	$var=explode("+", $key);
+    	 	var_dump($t[0]['Use_ID']);
+    	 	//判断是否打分过
+    	 	$res=Db::query("select * from grade where Dafen_ID='$dafen' and Project='$var[0]' and Beidafen_ID='$var[1]'");
+    	 	if(empty($res)){
+    	 		Db::execute("insert grade (Project,Beidafen_ID,Dafen_ID,Grade) values ('$var[0]','$var[1]',$dafen,$value)");
+    	 	}else{
+    	 		Db::execute("")
+    	 	}
+    	 }
     }
 }
